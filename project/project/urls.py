da8e0acbed5b9a72f78ab68bd1ced1d339c7aedf
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-12-11 00:59:58 +0100

    Update

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('orders/', include('orders.urls')),
     path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
 ]
