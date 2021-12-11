56b380e58f4096bc2aa6ca6ffb5b9b8b5c043bb2
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-12-11 15:40:48 +0100

    Create views #3

--- project/employees/views.py
+++ project/employees/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Employee, Position
+
 
 # Create your views here.
+class EmployeeListView(generic.ListView):
+    model = Employee
+    paginate_by = 20
+    template_name = 'products/base_list.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Employees'
+        return context
+
+
+class EmployeeDetailView(generic.DetailView):
+    model = Employee
+
+
+class PositionListView(generic.ListView):
+    model = Position
+    paginate_by = 20
+    template_name = 'products/base_list.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Positions'
+        return context
+
+
+class PositionDetailView(generic.DetailView):
+    model = Position
