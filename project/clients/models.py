33d0f05a88f6e8b7b530cbd1bf3f8b1adae87af9
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-12-11 16:06:31 +0100

    Create model #4

--- project/clients/models.py
+++ project/clients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=9)
+    email = models.EmailField(blank=True, null=True)
+    discount_card_code = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
