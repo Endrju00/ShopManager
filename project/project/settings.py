4c3dfae07a9d3b1a0f15b811bab8e7e00292894a
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-12-11 16:07:20 +0100

    Update files

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clients.apps.ClientsConfig',
     'employees.apps.EmployeesConfig',
     'orders.apps.OrdersConfig',
     'products.apps.ProductsConfig',
