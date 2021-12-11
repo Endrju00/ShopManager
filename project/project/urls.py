f638a3961defe951c69676ceb08dfd73f23bcd32
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-12-11 19:29:15 +0100

    Add and configure aboutpage #5

--- project/project/urls.py
+++ project/project/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='homepage'),
+    path('about/', views.AboutPageView.as_view(), name='aboutpage'),
     path('clients/', include('clients.urls')),
     path('employees/', include('employees.urls')),
     path('orders/', include('orders.urls')),
