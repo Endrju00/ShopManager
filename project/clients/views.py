6b4d4c2697f405f63c8f99b67b57c884d320ed1b
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-12-11 17:53:10 +0100

    Update views templates

--- project/clients/views.py
+++ project/clients/views.py
@@ -8,7 +8,7 @@ from .models import Client
 class ClientListView(generic.ListView):
     model = Client
     paginate_by = 20
-    template_name = 'products/base_list.html'
+    template_name = 'list.html'
 
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
